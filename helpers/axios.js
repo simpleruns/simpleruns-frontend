d5b972e025b2116242da0fe6a0844cc0a8240ab0
SincereDev <sinceresolution36@gmail.com>
2023-05-06 07:45:34 +0900

    https security issue fixed

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const instance = axios.create({
-    baseURL: 'http://170.64.154.214/api',
+    baseURL: 'https://170.64.154.214/api',
     timeout: 10000,
     headers: { 'Access-Control-Allow-Origin': '*' }
 });
