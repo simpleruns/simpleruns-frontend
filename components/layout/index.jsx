23add21f83a32654835a2b4e6805a306bfe93fe1
SincereDev <sinceresolution36@gmail.com>
2023-05-05 18:20:28 +0900

    deleted unnused pages

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Sidebar from "./sidebar";
 
 const Layout = (props) => {
-	const [sidebarOpen, setSidebarOpen] = useState(false);
-
 	return (
 		<>
 			<div className="flex h-full w-full">
-				<Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
+				<Sidebar />
 				<div className="h-full w-full bg-lightPrimary dark:!bg-navy-800">
 					<main
 						className={`mx-[12px] h-full flex-none transition-all md:pr-2 xl:ml-[313px]`}
--- components/layout/sidebar.jsx
+++ components/layout/sidebar.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 // define a NavItem prop
-import { BsGrid, BsUiChecksGrid, BsTruck, BsPeople } from "react-icons/bs";
+import { BsArrowRightShort, BsArrowLeftShort, BsUiChecksGrid, BsTruck, BsPeople } from "react-icons/bs";
 import { SlBriefcase, SlLogout, SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 // add NavItem prop to component prop
@@ -23,37 +23,67 @@ const Sidebar = () => {
 		Cookies.remove('x-access-token');
 		router.push('/auth/login');
 	}
+
+	function toggleSidebarHandler(e) {
+		e.preventDefault();
+		setSidebarOpen(!sidebarOpen);
+	}
+
 	return (
-		<aside id="logo-sidebar" className={`sm:none duration-175 linear fixed !z-50 flex min-h-full w-64 flex-col bg-white shadow-2xl shadow-white/5 transition-all dark:!bg-navy-900 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 overflow-y-scroll h-screen simple-scroller ${sidebarOpen ? "translate-x-0" : "-translate-x-96"}`} aria-label="Sidebar">
-			<div className="relative h-full px-3 py-4">
-				<Link href="#"
-					className="flex items-center pl-2.5 py-1.5 mb-2.5 rounded-3xl bg-white">
+		<aside id="logo-sidebar" className={`sm:none duration-175 linear fixed !z-50 flex min-h-full flex-col bg-white shadow-2xl shadow-white/5 transition-all dark:!bg-navy-900 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 overflow-visible h-screen simple-scroller ${sidebarOpen ? "w-64" : "w-[6rem]"}`} aria-label="Sidebar">
+			<div className="relative h-full px-3 pt-10 pb-4 overflow-y-scroll simple-scroller">
+				<Link href="/"
+					className={classNames({
+						" flex-col !py-3": !sidebarOpen,
+						"flex items-center py-1.5 mb-2.5 rounded-lg bg-white": true,
+					})}>
 					<Image
 						src="/assets/img/logo.png"
 						width={1366}
 						height={768}
 						style={{ width: 'auto' }}
-						className="h-[40px] mr-2" alt="Simpleruns Logo"
+						className="h-[40px]" alt="Simpleruns Logo"
 					/>
-					<span className="self-center text-lg font-semibold whitespace-nowrap text-blue-800">Simpleruns</span>
+					<span className={classNames({
+						" !text-xs": !sidebarOpen,
+						"self-center text-lg font-semibold whitespace-nowrap text-blue-800 transition": true,
+					})}>Simpleruns</span>
 				</Link>
-				<ul className="space-y-2 mt-10">
+				<ul className="space-y-2 mt-10 mb-10">
 					<li>
 						<Link href="/" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:text-white hover:bg-blue-500 dark:hover:bg-gray-700">
-							<BsUiChecksGrid size={24} />
-							<span className="ml-3">Dashboard</span>
+							<BsUiChecksGrid
+								size={24}
+								className={classNames({
+									" mx-auto my-3": !sidebarOpen,
+									"": true,
+								})}
+							/>
+							<span className={classNames({
+								" hidden": !sidebarOpen,
+								"ml-3": true,
+							})}>Dashboard</span>
 						</Link>
 					</li>
 
 					<li>
 						<button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-blue-500 hover:text-white dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown" data-collapse-toggle="dropdown" onClick={() => setOpenStateOne((prev) => !prev)}>
-							<BsPeople size={26} />
-							<span className="flex-1 ml-3 text-left whitespace-nowrap" >Drivers</span>
-							{openStateOne ? <SlArrowUp size={10} /> :
-								<SlArrowDown size={10} />}
+							<BsPeople
+								size={26}
+								className={classNames({
+									" mx-auto my-3": !sidebarOpen,
+									"": true,
+								})}
+							/>
+							<span className={classNames({
+								" hidden": !sidebarOpen,
+								"flex-1 ml-3 text-left whitespace-nowrap": true,
+							})}>Drivers</span>
+							{sidebarOpen ? openStateOne ? <SlArrowUp size={10} /> :
+								<SlArrowDown size={10} /> : ''}
 						</button>
 						<ul id="dropdown" className={classNames({
-							"  hidden": !openStateOne,
+							" hidden": !openStateOne || !sidebarOpen,
 							"py-2 space-y-2 transition-all duration-300": true,
 						})}>
 							<li>
@@ -70,14 +100,23 @@ const Sidebar = () => {
 
 					<li>
 						<button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-blue-500 hover:text-white dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown" data-collapse-toggle="dropdown" onClick={() => setOpenStateTwo((prev) => !prev)}>
-							<SlBriefcase size={26} />
-							<span className="flex-1 ml-3 text-left whitespace-nowrap" >Customers</span>
+							<SlBriefcase
+								size={26}
+								className={classNames({
+									" mx-auto my-3": !sidebarOpen,
+									"": true,
+								})}
+							/>
+							<span className={classNames({
+								" hidden": !sidebarOpen,
+								"flex-1 ml-3 text-left whitespace-nowrap": true,
+							})}>Customers</span>
 							{
-								openStateTwo ? <SlArrowUp size={10} /> :
-									<SlArrowDown size={10} />}
+								sidebarOpen ? openStateTwo ? <SlArrowUp size={10} /> :
+									<SlArrowDown size={10} /> : ''}
 						</button>
 						<ul id="dropdown" className={classNames({
-							"hidden": !openStateTwo,
+							" hidden": !openStateTwo || !sidebarOpen,
 							"py-2 space-y-2": true,
 						})}>
 							<li>
@@ -94,13 +133,22 @@ const Sidebar = () => {
 
 					<li>
 						<button type="button" className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-blue-500 hover:text-white dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown" data-collapse-toggle="dropdown" onClick={() => setOpenStateThree((prev) => !prev)}>
-							<BsTruck size={26} />
-							<span className="flex-1 ml-3 text-left whitespace-nowrap" >Trucks</span>
-							{openStateThree ? <SlArrowUp size={10} /> :
-								<SlArrowDown size={10} />}
+							<BsTruck
+								size={26}
+								className={classNames({
+									" mx-auto my-3": !sidebarOpen,
+									"": true,
+								})}
+							/>
+							<span className={classNames({
+								" hidden": !sidebarOpen,
+								"flex-1 ml-3 text-left whitespace-nowrap": true,
+							})}>Vehicles</span>
+							{sidebarOpen ? openStateThree ? <SlArrowUp size={10} /> :
+								<SlArrowDown size={10} /> : ''}
 						</button>
 						<ul id="dropdown" className={classNames({
-							"hidden": !openStateThree,
+							"hidden": !openStateThree || !sidebarOpen,
 							"py-2 space-y-2": true,
 						})}>
 							<li>
@@ -117,12 +165,29 @@ const Sidebar = () => {
 
 					<li>
 						<Link onClick={logoutHandler} href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-white dark:text-white hover:bg-blue-500 dark:hover:bg-gray-700">
-							<SlLogout size={26} />
+							<SlLogout
+								size={26}
+								className={classNames({
+									" mx-auto my-3": !sidebarOpen,
+									"": true,
+								})}
+							/>
 
-							<span className="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
+							<span className={classNames({
+								" hidden": !sidebarOpen,
+								"flex-1 ml-3 whitespace-nowrap": true,
+							})}>Sign Out</span>
 						</Link>
 					</li>
 				</ul>
+			</div>
+
+			<div className="absolute flex items-center justify-center text-white w-[30px] h-[30px] top-[50px] right-0 rounded-full bg-blue-900 translate-x-1/2 !z-50 md:!z-50 lg:!z-50 xl:!z-0" onClick={toggleSidebarHandler}>
+				{
+					sidebarOpen ?
+						<BsArrowLeftShort size={20} />
+						: <BsArrowRightShort size={20} />
+				}
 			</div>
 		</aside >
 	);
