1c590273a8a10cfa834ec0e1ce1c1012a4e8a8ae
SincereDev <sinceresolution36@gmail.com>
2023-06-06 08:27:59 +0900

    solved profile setting error in authentication pages

--- components/settingColor.jsx
+++ components/settingColor.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { RiMoonFill, RiSunFill } from "react-icons/ri";
 import { useRouter } from "next/router";
+import Cookies from 'js-cookie';
 
 import { instance } from 'helpers/axios';
 import { idAtom } from "helpers/authorize";
@@ -11,6 +12,7 @@ export default function SettingColor(props) {
     const [data, setData] = useState(null);
     const router = useRouter();
     const [darkmode, setDarkmode] = useState(false);
+    const token = Cookies.get('token');
 
     function changeThemeHandler() {
         setDarkmode(!darkmode);
@@ -26,7 +28,7 @@ export default function SettingColor(props) {
     }, [darkmode]);
 
     useEffect(() => {
-        user && instance.get(`/settings/user/${user}`)
+        user && token && instance.get(`/settings/user/${user}`)
             .then((res) => {
                 setData({
                     avatar: res.data.avatar,
@@ -46,7 +48,7 @@ export default function SettingColor(props) {
     }
 
     return (
-        data && <div class="mt-[3px] flex h-[61px] w-[150px] backdrop-blur-xl flex-grow items-center justify-around gap-2 rounded-full px-2 py-2 shadow-xl shadow-shadow-500 dark:shadow-none md:w-[150px] md:flex-grow-0 md:gap-1 xl:w-[150px] xl:gap-2 fixed top-[30px] right-[35px] !z-[99]">
+        <div className={`mt-[3px] flex h-[61px] backdrop-blur-xl flex-grow items-center justify-around gap-2 rounded-full px-2 py-2 shadow-xl shadow-shadow-500 dark:shadow-none md:flex-grow-0 md:gap-1 xl:gap-2 fixed top-[30px] right-[35px] !z-[99] ${data ? "w-[150px]" : "w-[61px]"}`}>
             <button
                 className="border-px flex h-[40px] w-[40px] items-center justify-center rounded-full border-[#6a53ff] bg-gradient-to-br from-brandLinear to-blueSecondary p-0"
                 onClick={changeThemeHandler}
@@ -59,24 +61,26 @@ export default function SettingColor(props) {
                     )}
                 </div>
             </button>
-            <div class="relative flex">
-                <div class="flex user-avatar z-[50]">
-                    <img class="h-10 w-10 rounded-full object-cover" src={data.avatar.url} alt="User Avatar" /></div>
-                <div class="py-2 top-8 -left-[180px] w-max absolute z-10 origin-top-right transition-all duration-300 ease-in-out scale-100 user-profile-dropdown">
-                    <div class="flex w-56 flex-col justify-start rounded-[5px] bg-white bg-cover bg-no-repeat shadow-xl shadow-shadow-500 dark:!bg-navy-700 dark:text-white dark:shadow-none">
-                        <div class="mt-3 ml-4">
-                            <div class="flex items-center gap-2">
-                                <p class="text-sm font-bold text-navy-700 dark:text-white">👋 Hey, {data.firstname + ' ' + data.lastname}</p>
+            {
+                data && <div class="relative flex">
+                    <div class="flex user-avatar z-[50]">
+                        <img class="h-10 w-10 rounded-full object-cover" src={data.avatar.url} alt="User Avatar" /></div>
+                    <div class="py-2 top-8 -left-[180px] w-max absolute z-10 origin-top-right transition-all duration-300 ease-in-out scale-100 user-profile-dropdown">
+                        <div class="flex w-56 flex-col justify-start rounded-[5px] bg-white bg-cover bg-no-repeat shadow-xl shadow-shadow-500 dark:!bg-navy-700 dark:text-white dark:shadow-none">
+                            <div class="mt-3 ml-4">
+                                <div class="flex items-center gap-2">
+                                    <p class="text-sm font-bold text-navy-700 dark:text-white">👋 Hey, {data.firstname + ' ' + data.lastname}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="mt-3 h-px w-full bg-gray-200 dark:bg-white/20 "></div>
-                        <div class="mt-3 ml-4 pb-3 flex flex-col">
-                            <a href="/settings/profile" class="text-sm text-gray-800 dark:text-white hover:dark:text-white">Profile Settings</a>
-                            <a href="/" class="mt-3 text-sm font-medium text-red-500 hover:text-red-500" onClick={logoutHandler}>Sign Out</a>
+                            <div class="mt-3 h-px w-full bg-gray-200 dark:bg-white/20 "></div>
+                            <div class="mt-3 ml-4 pb-3 flex flex-col">
+                                <a href="/settings/profile" class="text-sm text-gray-800 dark:text-white hover:dark:text-white">Profile Settings</a>
+                                <a href="/" class="mt-3 text-sm font-medium text-red-500 hover:text-red-500" onClick={logoutHandler}>Sign Out</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }
