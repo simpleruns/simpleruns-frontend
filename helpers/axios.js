0d90df6fa05de8a25ae33dd8bfa44b35458308c1
softwinner813 <geniusdev0813@gmail.com>
2023-07-05 00:22:33 -0700

    setup ENV

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,8 +1,16 @@
+
 import axios from 'axios';
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+if (process.env.NODE_ENV === 'production') {
+    require('dotenv').config({ path: '.env' });
+} else {
+    require('dotenv').config({ path: '.env.development' });
+}
+console.log(process.env.BACKEND_SERVER_URL);
 export const instance = axios.create({
     // baseURL: 'https://simpleruns-backend.vercel.app/api',
-    baseURL: 'http://170.64.154.214/api',
-    // baseURL: 'http://localhost:4000/api',
+    // baseURL: 'http://170.64.154.214/api',
+    baseURL: process.env.BACKEND_SERVER_URL,
     timeout: 50000,
     headers: { 'Access-Control-Allow-Origin': '*' }
 });
